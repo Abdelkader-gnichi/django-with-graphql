0c78d6202ead2561ae168408f2492b7afb27edbb
Abdelkader-gnichi <ggnichi097@gmail.com>
2025-03-25 12:12:25 +0100

    create some models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+import uuid
+from django.utils import timezone
 # Create your models here.
+
+class BaseModel(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Book(BaseModel):
+
+    name = models.CharField(max_length=100)
+    excerpt = models.TextField()
+
+    def __str__(self):
+        return f'{self.name}'
